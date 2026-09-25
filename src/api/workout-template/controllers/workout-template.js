1b5aba0414493af4545d163f254f1f083dfc5989
Maxime <maximekassam@msn.com>
2026-09-25 11:30:12 +0200

    published instead of draft

--- src/api/workout-template/controllers/workout-template.js
+++ src/api/workout-template/controllers/workout-template.js
@@ -46,6 +46,7 @@ module.exports = createCoreController(
                 description: description,
                 program: documentId,
               },
+              status: "published",
             });
 
             return workoutTemplate;
